15c569487f27ed8cde49541698bc8b3394697283
Nicholas A. Nevins <nevins.nicholas@gmail.com>
2025-09-20 20:43:19 -0600

    Touch up
    Just fixed a few small formatting errors

--- modules/dice_view.py
+++ modules/dice_view.py
@@ -1,16 +1,19 @@
 import discord
 
+
 class MyView(discord.ui.View):
-        def __init__(self, ctx):
-            super().__init__()
-            # You can add persistent views here if needed for restarts
+    def __init__(self, ctx):
+        super().__init__()
+        # You can add persistent views here if needed for restarts
 
-            emoji = ctx.guild.fetch_emoji(":1Roll:")
+        #emoji = ctx.guild.fetch_emoji(":1Roll:")
 
-            self.add_item(discord.ui.Button(label="d4", row=0))
-            self.add_item(discord.ui.Button(label="d6", row=0))
-            self.add_item(discord.ui.Button(label="d8", row=0))
+        self.add_item(discord.ui.Button(label="d4", row=0))
+        self.add_item(discord.ui.Button(label="d6", row=0))
+        self.add_item(discord.ui.Button(label="d8", row=0))
 
-        @discord.ui.button(label="Click Me!", style=discord.ButtonStyle.primary, custom_id="my_button")
+        @discord.ui.button(label="Click Me!", 
+                           style=discord.ButtonStyle.primary, 
+                           custom_id="my_button")
         async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
             await interaction.response.send_message("You clicked the button!", ephemeral=True)
